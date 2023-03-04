0c16fdae212eaac4cf5a80101b1a2a4ee5d1cb25
Ekankth <126363431+Ekankth@users.noreply.github.com>
2023-03-05 02:44:32 +0530

    Create asupantod.py

---
+++ BlackWeb/utils/asupantod.py
@@ -0,0 +1,90 @@
+from asyncio import *
+from random import *
+from pyrogram import *
+from pyrogram.types import *
+from BlackWeb.helper.admin import *
+from BlackWeb.helper.misc import *
+
+from pykillerx.helper.basic import *
+from pykillerx.helper import *
+from pykillerx.blacklist import *
+from pykillerx.help import *
+
+from config import *
+
+async def asupan_channel(client, message):
+    if message.chat.id == -1001554560763:
+        return await edit_or_reply(message, "**This command is prohibited from being used in this group**")
+    ren = await edit_or_reply(message, "`Wait a moment...`")
+    await gather(
+        ren.delete(),
+        client.send_video(
+            message.chat.id,
+            choice(
+                [
+                    asupan.video.file_id
+                    async for asupan in client.search_messages(
+                        "punyakenkan", filter=enums.MessagesFilter.VIDEO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+
+async def ayang_channel(client, message):
+    yanto = await message.reply("🔎 `Search Ayang...`")
+    pop = message.from_user.first_name
+    ah = message.from_user.id
+    await message.reply_photo(
+        choice(
+            [
+                lol.photo.file_id
+                async for lol in client.search_messages(
+                    "CeweLogoPack", filter=enums.MessagesFilter.PHOTO
+                )
+            ]
+        ),
+        False,
+        caption=f"Ayangnya [{pop}](tg://user?id={ah}) 💝",
+    )
+
+    await yanto.delete()
+
+async def ppcp_channel(client, message):
+    yanto = await message.reply("🔎 `Search PP Couple...`")
+    message.from_user.first_name
+    message.from_user.id
+    await message.reply_photo(
+        choice(
+            [
+                lol.photo.file_id
+                async for lol in client.search_messages(
+                    "ppcpcilik", filter=enums.MessagesFilter.PHOTO
+                )
+            ]
+        ),
+        False,
+        caption=f"📌 PP Couple nya Nih Kak",
+    )
+
+    await yanto.delete()
+
+async def ppanime_channel(client, message):
+    yanto = await message.reply("🔎 `Search PP Anime...`")
+    message.from_user.first_name
+    message.from_user.id
+    await message.reply_photo(
+        choice(
+            [
+                lol.photo.file_id
+                async for lol in client.search_messages(
+                    "animehikarixa", filter=enums.MessagesFilter.PHOTO
+                )
+            ]
+        ),
+        False,
+        caption=f"📌 PP Anime nya Nih Kak",
+    )
+
+    await yanto.delete()
